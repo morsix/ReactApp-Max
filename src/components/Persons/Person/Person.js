615a87869d210a8ce5fe45aed680f7e88fb26562
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-12 14:43:54 +0100

    Aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Aux from '../../../hoc/Auxiliary';
+
 import './Person.css';
 
 class Person extends Component {
@@ -7,13 +9,13 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...')
         return (
-            <div className="Person">
+            <Aux>
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text"
                     onChange={this.props.changed}
                     value={this.props.name} />
-            </div>
+            </Aux>
         )
     }
 };
