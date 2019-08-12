d4f7cd4bb7b62ff940020d6930b9222e9a024428
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-12 14:21:53 +0100

    pure component instead of checking every prop in shouldComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
   //   return state;
   // }
 
-  shouldComponentUpdate(nextprops, nextState){
-    console.log('[perons.js] shouldComponentUpdate')
-    if(nextprops.persons !== this.props.persons){
+  // shouldComponentUpdate(nextprops, nextState){
+  //   console.log('[perons.js] shouldComponentUpdate')
+  //   if(nextprops.persons !== this.props.persons){
 
-      return true;
-    }else{
-      return false;
-    }
-  }
+  //     return true;
+  //   }else{
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate(preProps, prevState){
     console.log('[Persons.js] getSnapshotBeforeUpdate');
