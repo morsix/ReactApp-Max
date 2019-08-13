c4696441d4a7b52fe3836b563a58fc0244ec052b
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-13 10:04:30 +0100

    using context the good way

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 
     const toggleButtonRef = useRef(null);
+    const authContext = useContext(AuthContext);
+    console.log(authContext.authenticated);
 
     useEffect( () =>{
         console.log('[Cockpit.js] UseEffect');
@@ -56,11 +58,7 @@ const cockpit = (props) => {
             <button  ref={toggleButtonRef} style={style} onClick={props.clicked}>
                 Toggle Persons
             </button>
-            <AuthContext.Consumer>{
-                context => 
-                <button style={style} onClick={context.login}>Log in</button>
-            }
-            </AuthContext.Consumer>
+                <button style={style} onClick={authContext.login}>Log in</button>
         </div>
     );
 };
